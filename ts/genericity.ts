0a49a32cbbcb04786386c7197227b98d73c7d6c4
shiganfeng <2454729834@qq.com>
2021-11-02 08:21:45 +0800

    ts

--- ts/genericity.ts
+++ ts/genericity.ts
@@ -10,42 +10,66 @@
 
 //泛型，可以支持不特定的数据类型  要求：传入参数和返回的参数一致
 //泛型函数
-function getData<T>(value:T):T{
-    return value
-}
+// function getData<T>(value:T):T{
+//     return value
+// }
 
-getData<number>(123)
-getData<string>('sgf')
+// getData<number>(123)
+// getData<string>('sgf')
 
-function getData1<T>(value:T):any{
-    return value
-}
+// function getData1<T>(value:T):any{
+//     return value
+// }
 
-getData1<number>(123)
-getData1<string>('sgf')
+// getData1<number>(123)
+// getData1<string>('sgf')
 
 //泛型类  例如需要同时支持返回数字和字符串两种类型
-class MinClass {
-    public list:number[] = [];
+// class MinClass {
+//     public list:number[] = [];
 
-    add(num:number){
-        this.list.push(num)
+//     add(num:number){
+//         this.list.push(num)
+//     }
+
+//     min():any{
+//         let minNum = this.list.length ? this.list[0] : '数组为空'
+//         this.list.sort(function(a, b){
+//             return a-b
+//         })
+//         minNum = this.list.length ? this.list[0] : '数组为空'
+//         return minNum
+//     }
+// }
+
+// let mymin = new MinClass()
+// console.log(mymin.min())
+// mymin.add(4)
+// mymin.add(5)
+// mymin.add(1)
+// mymin.add(3)
+// console.log(mymin.min())
+
+
+//类的泛型
+class MinClass<T> {
+    public list:T[] = [];
+
+    add(value:T):void{
+        this.list.push(value)
     }
 
-    min():any{
-        let minNum = this.list.length ? this.list[0] : '数组为空'
-        this.list.sort(function(a, b){
+    min():T{
+        let minNum = this.list[0]
+        this.list.sort(function(a:any, b:any){
             return a-b
         })
-        minNum = this.list.length ? this.list[0] : '数组为空'
+        minNum = this.list[0]
         return minNum
     }
 }
 
-let mymin = new MinClass()
-console.log(mymin.min())
-mymin.add(4)
-mymin.add(5)
-mymin.add(1)
-mymin.add(3)
-console.log(mymin.min())
+let mymin = new MinClass<string>() //实例化类，并且制定了类的T代表的类型是string
+mymin.add('1')
+mymin.add('2')
+
